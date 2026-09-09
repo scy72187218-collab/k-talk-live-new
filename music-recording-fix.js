70b8e7d5539a2b36afca54467e65116c36578257
scy72187218-collab <scy72187218@gmail.com>
2026-09-10 06:22:53 +0900

    기기 연결 안정화 파일 새 버전만 연결

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -15,7 +15,7 @@
   load('solo-right-dedupe.js?v=20260909-solo-right2');
   load('video-audio-stop-on-leave.js?v=20260909-stop1');
   load('video-social-actions-visible.js?v=20260909-social1');
-  load('ktalk-device-stability-20260909.js?v=20260909-device2');
+  load('ktalk-device-stability-20260909.js?v=20260910-device3');
   load('seller-ads-center-loader.js?v=20260909-business1');
 })();
 
