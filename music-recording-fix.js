097f8f2a2c4c2d511d52472bbe52ff6566999c14
scy72187218-collab <scy72187218@gmail.com>
2026-09-09 12:34:56 +0900

    홈 동영상 점 세 개 메뉴 연결

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -12,4 +12,5 @@
   load('mobile-open-compat.js?v=20260909-mobile1');
   load('video-more-menu.js?v=20260909-video-more1');
   load('beauty-natural-upgrade.js?v=20260909-beauty2');
+  load('public-feed-three-dot.js?v=20260909-feedmore1');
 })();
