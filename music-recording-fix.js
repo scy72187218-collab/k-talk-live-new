ba324d5ff5a8a2340525501ca65b5e3fe91f6749
scy72187218-collab <scy72187218@gmail.com>
2026-09-09 20:10:26 +0900

    내 프로필 저장 동영상 표시 복구 다시 연결

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -15,6 +15,7 @@
   load('solo-right-dedupe.js?v=20260909-solo-right2');
   load('video-audio-stop-on-leave.js?v=20260909-stop1');
   load('video-social-actions-visible.js?v=20260909-social1');
+  load('profile-video-restore-20260908.js?v=20260909-profile-restore1');
 })();
 
 /* 촬영 화면의 편집효과 바로 아래 V(더보기) 버튼만 제거. 다른 버튼/기능은 건드리지 않음. */
