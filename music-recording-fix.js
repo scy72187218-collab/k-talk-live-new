8698d68d38c53cebc0e0e384d7d42610135cd71c
scy72187218-collab <scy72187218@gmail.com>
2026-09-09 12:25:51 +0900

    보정 업그레이드 실제 화면에 연결

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -11,4 +11,5 @@
   load('earnings-rooms-copy.js?v=20260909-earnings-restore1');
   load('mobile-open-compat.js?v=20260909-mobile1');
   load('video-more-menu.js?v=20260909-video-more1');
+  load('beauty-natural-upgrade.js?v=20260909-beauty2');
 })();
