7e63d66efadecc2e75c5773bc1f1c09bce870020
scy72187218-collab <scy72187218@gmail.com>
2026-09-11 16:55:33 +0900

    Refresh only guest 5 and 6 waveform cache

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -24,7 +24,7 @@
   load('vocal-enhancer.js?v=20260910-vocal1');
   load('interface-recording-audio-fix.js?v=20260910-interface2');
   load('karaoke-audio-quality.js?v=20260910-karaoke1');
-  load('waveform-individual-bars.js?v=20260911-wavebars4');
+  load('waveform-individual-bars.js?v=20260911-wavebars5');
 })();
 
 /* 촬영 화면의 편집효과 바로 아래 V(더보기) 버튼만 제거. 다른 버튼/기능은 건드리지 않음. */
