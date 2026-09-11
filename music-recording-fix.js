cfae712ae5b46f503cc909bfa5ee42383929213a
scy72187218-collab <scy72187218@gmail.com>
2026-09-11 19:06:44 +0900

    Refresh secret duplicate match removal script only

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -30,7 +30,7 @@
   load('room-person-layout-controls.js?v=20260911-layout2');
   load('room-person-layout-no-number.js?v=20260911-nonumber3');
   load('group9-approved-room.js?v=20260911-group9-2');
-  load('group9-general-button.js?v=20260911-general9-1');
+  load('group9-general-button.js?v=20260911-general9-2');
   load('level-rules.js?v=20260911-level1');
 })();
 
