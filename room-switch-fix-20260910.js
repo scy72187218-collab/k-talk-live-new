24debc9f9fad5b15e70116789ec7e2008e262f3e
scy72187218-collab <scy72187218@gmail.com>
2026-09-11 12:59:18 +0900

    Move secret room earnings box above gifts only

--- room-switch-fix-20260910.js
+++ room-switch-fix-20260910.js
@@ -183,7 +183,7 @@
     if(earn.parentNode!==main)main.appendChild(earn);
     earn.style.setProperty('position','absolute','important');
     earn.style.setProperty('right','6px','important');
-    earn.style.setProperty('bottom','126px','important');
+    earn.style.setProperty('bottom','190px','important');
     earn.style.setProperty('left','auto','important');
     earn.style.setProperty('width','160px','important');
     earn.style.setProperty('height','64px','important');
@@ -205,7 +205,7 @@
     st.id='ktSecretBottomOnlyStyle20260911';
     st.textContent=''
       +'.ktsecret-room .ktsecret-gifts{bottom:3px!important}'
-      +'@media(max-width:390px){.ktsecret-room .ktsecret-earn-row{right:5px!important;bottom:120px!important;width:145px!important;height:58px!important}.ktsecret-room .ktsecret-chat{right:154px!important;bottom:64px!important}.ktsecret-room .ktsecret-gifts{bottom:3px!important}}';
+      +'@media(max-width:390px){.ktsecret-room .ktsecret-earn-row{right:5px!important;bottom:184px!important;width:145px!important;height:58px!important}.ktsecret-room .ktsecret-chat{right:154px!important;bottom:64px!important}.ktsecret-room .ktsecret-gifts{bottom:3px!important}}';
     document.head.appendChild(st);
   }
 
