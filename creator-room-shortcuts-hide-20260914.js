63f4e3fa19e0bec227630efd676422b021f4f5e3
scy72187218-collab <scy72187218@gmail.com>
2026-09-14 00:35:39 +0900

    Hide creator room shortcut row only

---
+++ creator-room-shortcuts-hide-20260914.js
@@ -0,0 +1,9 @@
+/* K-Talk 촬영 화면: 하단 방송방 바로가기 버튼 줄만 숨김. 라이브 버튼/준비화면은 유지. */
+(function(){
+  if(window.__ktCreatorRoomShortcutsHide20260914)return;
+  window.__ktCreatorRoomShortcutsHide20260914=true;
+  var s=document.createElement('style');
+  s.id='ktCreatorRoomShortcutsHide20260914Style';
+  s.textContent='.creator .kt-creator-room-shortcuts{display:none!important;}';
+  document.head.appendChild(s);
+})();
