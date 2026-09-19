0c15cc0c69064847abf0a1d50033eb66469648cb
scy72187218-collab <scy72187218@gmail.com>
2026-09-19 16:26:52 +0900

    Load repaired live presence before broadcast helpers

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -40,7 +40,7 @@
   load('treasure-global-room.js?v=20260913-treasure3');
   load('message-recipient-picker.js?v=20260911-message1');
   load('host-face-like-reward.js?v=20260911-hostlike1');
-  load('live-presence.js?v=20260913-link2');
+  load('live-presence.js?v=20260919-11am-connectfix2');
   load('live-presence-watchdog.js?v=20260913-link2');
 })();
 
