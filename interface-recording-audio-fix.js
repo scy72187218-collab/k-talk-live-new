8ecf3dbdca9f43c949861bfe85a0d1fa65b91a01
scy72187218-collab <scy72187218@gmail.com>
2026-09-19 14:09:54 +0900

    Prefer 44.1k stereo input for first-reference sound

--- interface-recording-audio-fix.js
+++ interface-recording-audio-fix.js
@@ -25,7 +25,7 @@
       echoCancellation:false,
       noiseSuppression:false,
       autoGainControl:false,
-      sampleRate:{ideal:48000},
+      sampleRate:{ideal:44100},
       channelCount:{ideal:2}
     };
     var tries=[];
