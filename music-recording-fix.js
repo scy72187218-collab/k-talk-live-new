a80b7c26f2be71707c26a63560f24b61f23886b7
scy72187218-collab <scy72187218@gmail.com>
2026-09-22 09:08:32 +0900

    Refresh faster LIVE connection

--- music-recording-fix.js
+++ music-recording-fix.js
@@ -40,7 +40,7 @@
   load('treasure-global-room.js?v=20260913-treasure3');
   load('message-recipient-picker.js?v=20260911-message1');
   load('host-face-like-reward.js?v=20260911-hostlike1');
-  load('live-presence.js?v=20260921-all-end-video1');
+  load('live-presence.js?v=20260922-fast-connect1');
   load('live-presence-watchdog.js?v=20260921-live-steady-final1');
 })();
 
